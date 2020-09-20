902c46a86c69e1fef0ed61692e0b41315a9be636
Alexej Harm <alexej.h@xiphos.de>
2020-09-20 23:50:32 +0200

    added x64-linux-gcc triplet

--- triplets/install.cmake
+++ triplets/install.cmake
@@ -1,4 +1,5 @@
 file(COPY
   ${CMAKE_CURRENT_LIST_DIR}/x64-windows-ipo.cmake
   ${CMAKE_CURRENT_LIST_DIR}/x64-linux-ipo.cmake
+  ${CMAKE_CURRENT_LIST_DIR}/x64-linux-gcc.cmake
   DESTINATION ${CMAKE_CURRENT_LIST_DIR}/../..)
