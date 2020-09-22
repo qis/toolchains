a163cb8f3b4acf113c35894a9ba452f3fa985b65
Alexej Harm <alexej.h@xiphos.de>
2020-09-22 07:39:49 +0200

    do not install linux gcc triplet

--- triplets/install.cmake
+++ triplets/install.cmake
@@ -1,5 +1,4 @@
 file(COPY
-  ${CMAKE_CURRENT_LIST_DIR}/x64-windows-ipo.cmake
   ${CMAKE_CURRENT_LIST_DIR}/x64-linux-ipo.cmake
-  ${CMAKE_CURRENT_LIST_DIR}/x64-linux-gcc.cmake
+  ${CMAKE_CURRENT_LIST_DIR}/x64-windows-ipo.cmake
   DESTINATION ${CMAKE_CURRENT_LIST_DIR}/../..)
